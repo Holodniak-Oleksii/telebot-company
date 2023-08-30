ebce1267786db9dbc868c409522b606ba848515a
Oleksii Holodniak <holodniakwork@gmail.com>
2023-08-31 00:54:59 +0300

    add individual id

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require("express");
 require("dotenv").config();
-const app = express();
+const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const { instrument } = require("@socket.io/admin-ui");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const TelegramAPI = require("node-telegram-bot-api");
+
+const app = express();
 const bot = new TelegramAPI(process.env.TOKEN_API, { polling: true });
 
 app.use(cors());
@@ -17,30 +20,42 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+const contactIO = io.of("contact");
 
-bot.on("message", (message) => {
-  const userId = message.reply_to_message?.text?.split(" ")[3];
-  if (userId) {
-    io.to(userId).emit("receive_message", {
-      message: message.text,
-      id: message.message_id,
+async function start() {
+  try {
+    bot.on("message", (message) => {
+      const userId = message.reply_to_message?.text?.split(" ")[5];
+
+      if (userId) {
+        contactIO.to(userId).emit("receive_message", {
+          message: message.text,
+          id: uuidv4(),
+        });
+      }
     });
+
+    contactIO.on("connection", (socket) => {
+      socket.on("join_room", (data) => {
+        socket.join(data);
+      });
+
+      socket.on("send_message", async (data) => {
+        bot.sendMessage(
+          process.env.CHAT_ID,
+          `💬 Message from the user ${data.room} : ${data.message}`
+        );
+      });
+    });
+
+    server.listen(process.env.PORT, () => {
+      console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
   }
-});
+}
 
-io.on("connection", (socket) => {
-  socket.on("join_room", (data) => {
-    socket.join(socket.id);
-  });
-
-  socket.on("send_message", async (data) => {
-    bot.sendMessage(
-      process.env.CHAT_ID,
-      `Повідомлення від користувача ${socket.id} \n Текст: ${data.message}`
-    );
-  });
-});
-
-server.listen(process.env.PORT, () => {
-  console.log("SERVER IS RUNNING");
-});
+start();
+instrument(io, { auth: false });
